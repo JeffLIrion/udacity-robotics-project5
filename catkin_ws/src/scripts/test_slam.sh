0a0f8985bc8a985faf4730210998203a6b74554a
Jeff Irion <jefflirion@users.noreply.github.com>
2020-05-25 20:24:23 -0700

    Fix test_slam.sh and view_navigation.launch

--- catkin_ws/src/scripts/test_slam.sh
+++ catkin_ws/src/scripts/test_slam.sh
@@ -3,7 +3,7 @@
 SCRIPT_ROOT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Based on https://github.com/udacity/RoboND-SLAMLAb/blob/master/SLAM.sh
-xterm  -e "cd $SCRIPT_ROOT_DIRECTORY/../..; source devel/setup.bash ; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$SCRIPT_ROOT_DIRECTORY/map/worlds/project4b.world " &
+xterm  -e "cd $SCRIPT_ROOT_DIRECTORY/../..; source devel/setup.bash ; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$SCRIPT_ROOT_DIRECTORY/../map/worlds/project4b.world " &
 sleep 5
 xterm  -e "cd $SCRIPT_ROOT_DIRECTORY/../..; source devel/setup.bash ; roslaunch turtlebot_teleop keyboard_teleop.launch " &
 sleep 2
