c77d41ea26059c3a289fb83b4fcde877254e59dd
Jeff Irion <jefflirion@users.noreply.github.com>
2020-05-30 20:31:17 -0700

    Make add_markers.cpp and pick_objects.cpp communicate

--- catkin_ws/src/add_markers/src/add_markers.cpp
+++ catkin_ws/src/add_markers/src/add_markers.cpp
@@ -1,4 +1,6 @@
+#include <geometry_msgs/Pose.h>
 #include <ros/ros.h>
+#include <std_msgs/Bool.h>
 #include <visualization_msgs/Marker.h>
 
 constexpr char kNodeName[] = "add_markers";
@@ -7,83 +9,95 @@ constexpr char kMarkerNamespace[] = "udacity_project5";
 constexpr char kTopic[] = "visualization_marker";
 constexpr int kMarkerType = visualization_msgs::Marker::CUBE;
 
+// Topics from `pick_objects.cpp`
+constexpr char kGoalTopic[] = "/pick_objects_goal";
+constexpr char kSuccessTopic[] = "/pick_objects_success";
+constexpr int kSubscribeQueueSize = 10;
+
 class MarkerPublisher {
  public:
-  MarkerPublisher() { pub_ = node_handle_.advertise<visualization_msgs::Marker>(kTopic, 1); }
+  MarkerPublisher() {
+    pub_ = node_handle_.advertise<visualization_msgs::Marker>(kTopic, 1);
 
-  /**
-   * @brief Add a marker.
-   *
-   * @param x The x location of the marker
-   * @param y the y location of the marker
-   * @param marker_id The ID for the marker
-   */
-  void AddMarker(const double &x, const double &y, const int32_t &marker_id = 0) {
-    AddOrDeleteMarker(true, x, y, marker_id);
-  }
+    // Subscribe to topics published by `pick_objects.cpp`
+    goal_sub_ = node_handle_.subscribe(kGoalTopic, kSubscribeQueueSize, &MarkerPublisher::AddMarker, this);
+    success_sub_ = node_handle_.subscribe(kSuccessTopic, kSubscribeQueueSize, &MarkerPublisher::DeleteMarker, this);
 
-  /**
-   * @brief Delete a marker.
-   *
-   * @param x The x location of the marker
-   * @param y the y location of the marker
-   * @param marker_id The ID for the marker
-   */
-  void DeleteMarker(const double &x, const double &y, const int32_t &marker_id = 0) {
-    AddOrDeleteMarker(false, x, y, marker_id);
+    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+    marker_.header.frame_id = kFrameID;
+    marker_.header.stamp = ros::Time::now();
+
+    // Set the namespace and id for this marker.  This serves to create a unique ID
+    // Any marker sent with the same namespace and id will overwrite the old one
+    marker_.ns = kMarkerNamespace;
+    marker_.id = 0;
+
+    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+    marker_.type = kMarkerType;
+
+    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+    // marker_.action = add_marker ? visualization_msgs::Marker::ADD : visualization_msgs::Marker::DELETE;
+
+    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+    marker_.pose.position.x = 0.;
+    marker_.pose.position.y = 0.;
+    marker_.pose.position.z = 00;
+    marker_.pose.orientation.x = 0.0;
+    marker_.pose.orientation.y = 0.0;
+    marker_.pose.orientation.z = 0.0;
+    marker_.pose.orientation.w = 1.0;
+
+    // Set the scale of the marker -- 1x1x1 here means 1m on a side
+    marker_.scale.x = 1.0;
+    marker_.scale.y = 1.0;
+    marker_.scale.z = 1.0;
+
+    // Set the color -- be sure to set alpha to something non-zero!
+    marker_.color.r = 0.0f;
+    marker_.color.g = 1.0f;
+    marker_.color.b = 0.0f;
+    marker_.color.a = 1.0;
+
+    marker_.lifetime = ros::Duration();
   }
 
  private:
   ros::NodeHandle node_handle_;
   ros::Publisher pub_;
+  ros::Subscriber goal_sub_;
+  ros::Subscriber success_sub_;
+  visualization_msgs::Marker marker_;
 
   /**
    * @brief Add a marker.
    *
-   * @param add_marker If `true`, we will add the marker; otherwise, we will delete it
-   * @param x The x location of the marker
-   * @param y the y location of the marker
-   * @param marker_id The ID for the marker
+   * @param pose_msg The destination pose
    */
-  void AddOrDeleteMarker(const bool &add_marker, const double &x, const double &y, const int32_t &marker_id) {
-    visualization_msgs::Marker marker;
-    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-    marker.header.frame_id = kFrameID;
-    marker.header.stamp = ros::Time::now();
+  void AddMarker(const geometry_msgs::Pose& pose_msg) {
+    marker_.action = visualization_msgs::Marker::ADD;
+    marker_.pose.position.x = pose_msg.position.x;
+    marker_.pose.position.y = pose_msg.position.y;
+    PublishMarker();
+  }
 
-    // Set the namespace and id for this marker.  This serves to create a unique ID
-    // Any marker sent with the same namespace and id will overwrite the old one
-    marker.ns = kMarkerNamespace;
-    marker.id = marker_id;
+  /**
+   * @brief Delete a marker.
+   *
+   * @param bool_msg Whether or not the robot reached its destination.
+   */
+  void DeleteMarker(const std_msgs::Bool& bool_msg) {
+    // If the robot did not reach the destination, don't do anything
+    if (!bool_msg.data) {
+      return;
+    }
+    marker_.action = visualization_msgs::Marker::DELETE;
+    PublishMarker();
+  }
 
-    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
-    marker.type = kMarkerType;
-
-    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-    marker.action = add_marker ? visualization_msgs::Marker::ADD : visualization_msgs::Marker::DELETE;
-
-    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    marker.pose.position.x = x;
-    marker.pose.position.y = y;
-    marker.pose.position.z = 0;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
-
-    // Set the scale of the marker -- 1x1x1 here means 1m on a side
-    marker.scale.x = 1.0;
-    marker.scale.y = 1.0;
-    marker.scale.z = 1.0;
-
-    // Set the color -- be sure to set alpha to something non-zero!
-    marker.color.r = 0.0f;
-    marker.color.g = 1.0f;
-    marker.color.b = 0.0f;
-    marker.color.a = 1.0;
-
-    marker.lifetime = ros::Duration();
-
+  /**
+   * @brief Publish the marker.
+   */
+  void PublishMarker() {
     // Publish the marker
     while (pub_.getNumSubscribers() < 1) {
       if (!ros::ok()) {
@@ -92,13 +106,15 @@ class MarkerPublisher {
       ROS_WARN_ONCE("Please create a subscriber to the marker");
       sleep(1);
     }
-    pub_.publish(marker);
+    pub_.publish(marker_);
   }
 };
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   ros::init(argc, argv, kNodeName);
   MarkerPublisher marker_publisher;
 
-  marker_publisher.AddMarker(1, 2);
+  ros::spin();
+
+  return 0;
 }
--- catkin_ws/src/pick_objects/src/pick_objects.cpp
+++ catkin_ws/src/pick_objects/src/pick_objects.cpp
@@ -1,11 +1,18 @@
 #include <actionlib/client/simple_action_client.h>
+#include <geometry_msgs/Pose.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <ros/ros.h>
+#include <std_msgs/Bool.h>
 
 namespace project5 {
 constexpr char kNodeName[] = "pick_objects";
 constexpr char kActionName[] = "move_base";
 constexpr char kFrameID[] = "map";
+
+// Topics
+constexpr char kGoalTopic[] = "/pick_objects_goal";
+constexpr char kSuccessTopic[] = "/pick_objects_success";
+constexpr int kPublishQueueSize = 10;
 
 constexpr double x1 = 1.0;
 constexpr double y1 = 2.0;
@@ -19,43 +26,70 @@ constexpr double qw2 = 1.0;
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-/**
- * @brief Drive to the specified position.
- *
- * @param action_client The move base action client
- * @param x The x coordinate of the goal
- * @param y The y coordinate of the goal
- * @param qw The `q_w` component of the goal's quaternion orientation
- * @return `true` if successful; `false` otherwise
- */
-bool DriveToGoal(MoveBaseClient *action_client, const double &x, const double &y, const double &qw) {
-  move_base_msgs::MoveBaseGoal goal;
-
-  // set up the frame parameters
-  goal.target_pose.header.frame_id = project5::kFrameID;
-  goal.target_pose.header.stamp = ros::Time::now();
-
-  // Define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = x;
-  goal.target_pose.pose.position.y = y;
-  goal.target_pose.pose.orientation.w = qw;
-
-  // Send the goal position and orientation for the robot to reach
-  ROS_INFO("Sending goal");
-  action_client->sendGoal(goal);
-
-  // Wait an infinite time for the results
-  action_client->waitForResult();
-
-  // Check if the robot reached its goal
-  if (action_client->getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
-    ROS_INFO("Successfully drove to (%f, %f, %f)", x, y, qw);
-    return true;
+class PickObjects {
+ public:
+  PickObjects() {
+    // Inform ROS master that we will be publishing messages
+    goal_pub_ = node_handle_.advertise<geometry_msgs::Pose>(project5::kGoalTopic, project5::kPublishQueueSize);
+    success_pub_ = node_handle_.advertise<std_msgs::Bool>(project5::kSuccessTopic, project5::kPublishQueueSize);
   }
 
-  ROS_INFO("Failed to drive to (%f, %f, %f)", x, y, qw);
-  return false;
-}
+  /**
+   * @brief Drive to the specified position.
+   *
+   * @param action_client The move base action client
+   * @param x The x coordinate of the goal
+   * @param y The y coordinate of the goal
+   * @param qw The `q_w` component of the goal's quaternion orientation
+   * @return `true` if successful; `false` otherwise
+   */
+  bool DriveToGoal(MoveBaseClient *action_client, const double &x, const double &y, const double &qw) {
+    move_base_msgs::MoveBaseGoal goal;
+
+    // set up the frame parameters
+    goal.target_pose.header.frame_id = project5::kFrameID;
+    goal.target_pose.header.stamp = ros::Time::now();
+
+    // Define a position and orientation for the robot to reach
+    goal.target_pose.pose.position.x = x;
+    goal.target_pose.pose.position.y = y;
+    goal.target_pose.pose.orientation.w = qw;
+
+    // Publish the goal pose
+    geometry_msgs::Pose goal_pose;
+    goal_pose.position.x = x;
+    goal_pose.position.y = y;
+    goal_pose.orientation.w = qw;
+    goal_pub_.publish(goal_pose);
+
+    // Send the goal position and orientation for the robot to reach
+    ROS_INFO("Sending goal");
+    action_client->sendGoal(goal);
+
+    // Wait an infinite time for the results
+    action_client->waitForResult();
+
+    // Check if the robot reached its goal
+    if (action_client->getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+      ROS_INFO("Successfully drove to (%f, %f, %f)", x, y, qw);
+      std_msgs::Bool bool_msg;
+      bool_msg.data = true;
+      success_pub_.publish(bool_msg);
+      return true;
+    }
+
+    ROS_INFO("Failed to drive to (%f, %f, %f)", x, y, qw);
+    std_msgs::Bool bool_msg;
+    bool_msg.data = false;
+    success_pub_.publish(bool_msg);
+    return false;
+  }
+
+ private:
+  ros::NodeHandle node_handle_;
+  ros::Publisher goal_pub_;
+  ros::Publisher success_pub_;
+};
 
 int main(int argc, char **argv) {
   // Initialize the simple_navigation_goals node
@@ -64,8 +98,10 @@ int main(int argc, char **argv) {
   // Tell the action client that we want to spin a thread by default
   MoveBaseClient action_client(project5::kActionName, true);
 
+  PickObjects pick_objects;
+
   // Drive to the first goal
-  if (!DriveToGoal(&action_client, project5::x1, project5::y1, project5::qw1)) {
+  if (!pick_objects.DriveToGoal(&action_client, project5::x1, project5::y1, project5::qw1)) {
     return -1;
   }
 
@@ -73,7 +109,7 @@ int main(int argc, char **argv) {
   ros::Duration(5).sleep();
 
   // Drive to the second goal
-  if (!DriveToGoal(&action_client, project5::x2, project5::y2, project5::qw2)) {
+  if (!pick_objects.DriveToGoal(&action_client, project5::x2, project5::y2, project5::qw2)) {
     return -1;
   }
 
